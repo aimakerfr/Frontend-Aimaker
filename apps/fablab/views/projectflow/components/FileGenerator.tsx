f9357f8238c281ff0a5afcda3d961246255fbc2c
ManuelPrada <maapradase@unal.edu.co>
2026-02-18 09:58:29 -0500

    Add next/previous navigation and file selection support in `projectFlow`. Streamline maker path handling, centralize persistence, and extend translations.

--- apps/fablab/views/projectflow/components/FileGenerator.tsx
+++ apps/fablab/views/projectflow/components/FileGenerator.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 const FileGenerator: React.FC = () => {
+  // TODO: WHen mounting bring the maker_path_variables for this maker path using the prop makerPathId
+  // TODO: Create a logic for ensamble the index, given each variable, get the source url to inject in html
+  // TODO: use a button to trigger the file generation logic
+  // TODO: After the file generation, download the file.
   return <div />;
 };
 
